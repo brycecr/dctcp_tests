f8f9bc7b23572f9bccbbe70c410f21c076f57454
Bryce Cronkite-Ratcliff <brycecr@gmail.com>
2016-01-25 23:34:29 -0800

    small tweaks to improve run.fish

--- run.fish
+++ run.fish
@@ -1,7 +1,7 @@
-for x in (seq 2)
+for x in (seq 20)
 	sudo ./tcp_fair.sh 
 	for file in /tmp/wireshark*
-		sudo tshark -r $file -qz "io,stat,0,tcp&&ip.src==10.0.0.2,tcp&&ip.src==10.0.0.3,tcp&&ip.src==10.0.0.4,tcp&&ip.src==10.0.0.5,tcp&&ip.src==10.0.0.6,tcp&&ip.src==10.0.0.7,tcp&&ip.src==10.0.0.8,tcp&&ip.src==10.0.0.9,tcp&&ip.src==10.0.0.10,tcp&&ip.src==10.0.0.11,tcp&&ip.src==10.0.0.12" > throughput_output
+		sudo tshark -r $file -qz "io,stat,0,tcp&&ip.src==10.0.0.2&&frame.time_relative>10.0,tcp&&ip.src==10.0.0.3&&frame.time_relative>10.0,tcp&&ip.src==10.0.0.4&&frame.time_relative>10.0,tcp&&ip.src==10.0.0.5&&frame.time_relative>10.0,tcp&&ip.src==10.0.0.6&&frame.time_relative>10.0,tcp&&ip.src==10.0.0.7&&frame.time_relative>10.0,tcp&&ip.src==10.0.0.8&&frame.time_relative>10.0,tcp&&ip.src==10.0.0.9&&frame.time_relative>10.0,tcp&&ip.src==10.0.0.10&&frame.time_relative>10.0,tcp&&ip.src==10.0.0.11&&frame.time_relative>10.0,tcp&&ip.src==10.0.0.12&&frame.time_relative>10.0" > throughput_output
 		sed -f ./script/pass1.sed throughput_output | sed -f ./script/pass2.sed | tr '\n' ' ' > script/data/$x.txt
 		sudo rm $file
 	end
